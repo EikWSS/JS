138d964d0e861e5f2818196a07db6ebdc689973a
EikWSS <stubbenspille@gmail.com>
2024-11-08 15:01:26 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,111 @@
+// Oppgave 1
+var tekst1 = "Hei hei "
+var tekst2 = "Eik"
+
+console.log(tekst1 + tekst2);
+
+//oppgave2
+var tall = 5
+if (tall >= 0) {
+    console.log("tallet er positiv");
+    
+}
+else{
+
+    console.log("tallet er positiv");
+    
+}
+
+// Oppgave 3
+var tall1 = 7
+var tall2 = 5
+
+if (tall1 > tall2) {
+    console.log("tall1 er størst");
+    
+}
+ else if(tall1 == tall2) {
+    console.log("tall1 og tall2 har lik verdig");
+    
+ }
+
+ else{
+    console.log("tall2 er størst");
+    
+ }
+
+ //oppgave4
+
+ var tal1 = 10
+ var tal2 = 77
+ var tal3 = 80
+
+ if (tal1 > tal2 && tal1 > tal3) {
+    console.log("tal1 er størst");
+    
+ } else  if (tal2 > tal1 && tal2 > tal3) {
+    console.log("tall2 er størst");
+    
+ } 
+
+else if (tal3 > tal2 && tal3 > tal1) {
+    console.log("tall3 er størst");
+    
+ } 
+
+ //oppgave5
+
+ var Eik = 49
+ if (Eik >= 50) {
+  console.log("bestått");
+   
+ }
+ else{
+    console.log("ikke bestått");
+    
+ }
+
+
+ //oppgave6
+
+ var ukeDag = 1
+ if (ukeDag == 1) {
+    console.log("det er mandag");
+    
+ }
+
+else if (ukeDag == 2) {
+    console.log("det er tirsdag");
+    
+ }
+ else if (ukeDag == 3) {
+    console.log("det er onsdag");
+    
+ }
+ else if (ukeDag == 4) {
+    console.log("det er torsdag");
+    
+ }
+
+ else if (ukeDag == 5) {
+    console.log("det er fredag");
+    
+ }
+ else if (ukeDag == 6) {
+    console.log("det er lørdag");
+    
+ }
+ else if (ukeDag == 7) {
+    console.log("det er søndag");
+    
+ }
+
+ //oppgave 7
+
+ var Celsius = 10
+ var farenheight =(Celsius*1.8+32)
+ console.log(farenheight);
+
+ 
+ 
+ 
